e0474cfe7e3e2d79262bc9cbcc4be28b69d40b6a
zKitKato <gianluca.ruggieri05@gmail.com>
2025-05-15 10:07:00 +0200

    pom updated, created pagecontroller

---
+++ bibliothek-VerwaltungFrontend/src/main/java/net/kato/bibliothekVerwaltungFrontend/controller/PageController.java
@@ -0,0 +1,16 @@
+package net.kato.bibliothekVerwaltungFrontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+    @RequestMapping(value = {"/", "/home", "/index"})
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("title", "Bibliothek");
+        return mv;
+    }
+}
