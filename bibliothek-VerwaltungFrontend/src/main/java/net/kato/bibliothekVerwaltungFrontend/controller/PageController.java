a27abc9d56052a185416a9e8ccc5bec349b4c13e
zKitKato <gianluca.ruggieri05@gmail.com>
2025-05-18 16:53:58 +0200

    Created Frontend

--- bibliothek-VerwaltungFrontend/src/main/java/net/kato/bibliothekVerwaltungFrontend/controller/PageController.java
+++ bibliothek-VerwaltungFrontend/src/main/java/net/kato/bibliothekVerwaltungFrontend/controller/PageController.java
@@ -1,7 +1,9 @@
 package net.kato.bibliothekVerwaltungFrontend.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,6 +13,26 @@ public class PageController {
     public ModelAndView index() {
         ModelAndView mv = new ModelAndView("page");
         mv.addObject("title", "Bibliothek");
+        return mv;
+    }
+
+    /*@RequestMapping(value = "/test")
+    public ModelAndView test(@RequestParam(value="title", required = false)String greeting) {
+        if (greeting == null) {
+            greeting = "Welt!";
+        }
+        ModelAndView mv = new ModelAndView("page");
+        mv.addObject("title", greeting);
+        return mv;
+    }*/
+
+    @RequestMapping(value = "/test/{greeting}")
+    public ModelAndView test(@PathVariable("greeting") String greeting) {
+        if (greeting == null) {
+            greeting = "Welt!";
+        }
+        ModelAndView mv = new ModelAndView("page");
+        mv.addObject("title", greeting);
         return mv;
     }
 }
